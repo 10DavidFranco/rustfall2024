91d0935de80335f3a9ac02c972eec45547cfd462
10DavidFranco <147568740+10DavidFranco@users.noreply.github.com>
2024-09-16 15:45:45 +0000

    Concatenated String

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,5 +1,13 @@
+fn concat_strings(s1: &String, s2: &String) -> String {
+   let mut word = s1.to_string(); //Creating a new variable word, that is mutable...and assigned the value s1
+   word.push_str(s2); //Pushed back s2 string into s1 to concatenate word into one sentence.
+
+   return word; //Returning string variable word.
+}
+
 fn main() {
-   //mutability -> by default values are immutable
-   //shadowing -> variable scope is what is relevant
-   //data sizes are ABSOLUTE
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("World!");
+    let result = concat_strings(&s1, &s2);
+    println!("{}", result); // Should print: "Hello, World!"
 }
