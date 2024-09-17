7b023239667d90d26ed0f305869d66f68146a403
10DavidFranco <147568740+10DavidFranco@users.noreply.github.com>
2024-09-17 23:33:12 +0000

    Practice problem start

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,13 +1,5 @@
-fn concat_strings(s1: &String, s2: &String) -> String {
-   let mut word = s1.to_string(); //Creating a new variable word, that is mutable...and assigned the value s1
-   word.push_str(s2); //Pushed back s2 string into s1 to concatenate word into one sentence.
+fn main(){
+int const freeze = 32;
 
-   return word; //Returning string variable word.
-}
 
-fn main() {
-    let s1 = String::from("Hello, ");
-    let s2 = String::from("World!");
-    let result = concat_strings(&s1, &s2);
-    println!("{}", result); // Should print: "Hello, World!"
 }
